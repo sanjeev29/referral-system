3e7224c692111a3bd23ef7a789eeaed3a60790a4
Sanjeev Raichur <sanjeevraichur29@gmail.com>
2021-06-20 01:42:26 +0530

    Add Profile model and register it with django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Admin options for Profile model
+    """
+    list_display = ('id', 'user', 'code', 'referred_by', 'created_at', 'updated_at')
+
+
+admin.site.register(Profile, ProfileAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    """
+    User profile model
+    """
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=12, blank=True)
+    referred_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='ref_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def save(self, *args, **kwargs):
+        if self.code == "":
+            self.code = uuid.uuid4().hex[:12]
+
+        super(Profile, self).save(*args, **kwargs)
