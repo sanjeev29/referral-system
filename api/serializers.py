d243b557c8ed378afbb74dc1313a6e770902e54a
Sanjeev Raichur <sanjeevraichur29@gmail.com>
2021-06-20 14:47:38 +0530

    Add profile model serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from api.models import Invitation
+from api.models import Invitation, Profile
 
 
 class InvitationSerializer(serializers.ModelSerializer):
@@ -11,8 +11,8 @@ class InvitationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Invitation
-        fields = ['email', 'link', 'created_by', 'created_at', 'updated_at']
-        read_only_fields = ['link', 'created_by']
+        fields = ['email', 'link', 'created_at', 'updated_at']
+        read_only_fields = ['link']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,3 +24,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Profile model serializer
+    """
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'email']
